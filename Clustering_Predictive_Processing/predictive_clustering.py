c85e35c7d4eba0b7a329dcca51da614738c1bace
ManuIluro22 <manuelarnaufernandez@gmail.com>
2024-03-07 17:09:45 +0100

    Updating Clustering Time Series and predictive_plots, utils

--- Clustering_Predictive_Processing/predictive_clustering.py
+++ Clustering_Predictive_Processing/predictive_clustering.py
@@ -8,38 +8,87 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
-def hierarchical_clustering_ts(list_df,metric,params,plotting = True):
+def hierarchical_clustering_ts(list_df, metric, params, plotting=True):
+    """
+    Performs hierarchical clustering on time series data and optionally plots the dendrogram.
+
+    Parameters:
+    - list_df: A list of DataFrames containing time series data.
+    - metric: A function to compute the distance matrix from time series data.
+    - params: Parameters for the linkage method used in hierarchical clustering.
+    - plotting: Boolean indicating whether to plot the dendrogram.
+
+    Returns:
+    - Z: The hierarchical clustering encoded as a linkage matrix.
+    """
+
     dist_mat_list = []
 
+    # Compute distance matrices for each DataFrame in list_df
     for df in list_df:
-        ts_data= df.astype(np.float64)
-        distance_matrix = metric(ts_data)
-        dist_mat_list.append(distance_matrix.astype(np.float64))
+        ts_data = df.astype(np.float64)  # Ensure data is in float64 format
+        distance_matrix = metric(ts_data)  # Compute distance matrix
+        dist_mat_list.append(distance_matrix.astype(np.float64))  # Store distance matrix
+
+    # Combine distance matrices if there are multiple, otherwise use the single matrix
     if len(dist_mat_list) == 1:
         condensed_dist_matrix = squareform(dist_mat_list[0])
     else:
-        condensed_dist_matrix = squareform(np.mean(dist_mat_list,axis=0))
+        # Average distance matrices if there's more than one
+        condensed_dist_matrix = squareform(np.mean(dist_mat_list, axis=0))
 
-
-    # Ensure params is a dictionary, even if None
+    # Perform hierarchical clustering
     Z = linkage(condensed_dist_matrix, **params)
 
-    if plotting == True:
-        #plot_time_series(dendrogram(Z),title = "Hierarchical Clustering Dendrogram")
+    # Plot dendrogram if plotting is enabled
+    if plotting:
         plt.figure(figsize=(10, 7))
         dendrogram(Z)
         plt.title('Hierarchical Clustering Dendrogram')
         plt.xlabel('Time Series Index')
         plt.ylabel('Distance')
         plt.show()
-        # %%
+
     return Z
 
 
+def create_cluster_df(linkage, df, params):
+    """
+    Creates a DataFrame with 'Subject ID' and cluster assignments obtained from hierarchical clustering.
 
-def create_cluster_df(linkage,df,params):
+    Parameters:
+    - linkage: Linkage matrix from hierarchical clustering.
+    - df: Original DataFrame containing 'Subject ID'.
+    - params: Parameters for forming flat clusters.
+
+    Returns:
+    - DataFrame with 'Subject ID' and corresponding cluster assignments.
+    """
+
+    # Form flat clusters from the hierarchical clustering
     clusters_df = fcluster(linkage, **params)
-    df.reset_index(inplace=True)
-    df.loc[:, "clusters"] = clusters_df.copy()
+    df.reset_index(inplace=True)  # Reset index of the original DataFrame
+    df.loc[:, "clusters"] = clusters_df.copy()  # Assign cluster labels to 'clusters' column
 
-    return df[["Subject ID", "clusters"]].copy()
+    return df[["Subject ID", "clusters"]].copy()  # Return DataFrame with 'Subject ID' and 'clusters'
+
+
+def length_cluster(df_cluster):
+    """
+    Counts the number of occurrences for each cluster.
+
+    Parameters:
+    - df_cluster: DataFrame containing cluster assignments.
+
+    Returns:
+    - DataFrame with each cluster and its corresponding count.
+    """
+
+    # Count occurrences of each cluster
+    unique_clusters, counts = np.unique(df_cluster["clusters"], return_counts=True)
+    cluster_counts_df = pd.DataFrame({
+        'Cluster': unique_clusters,
+        'Count': counts
+    })
+
+    return cluster_counts_df
--- Clustering_Predictive_Processing/utils.py
+++ Clustering_Predictive_Processing/utils.py
@@ -8,45 +8,76 @@ import seaborn as sns
 import predictive_plots
 
 def filter_data(df,data_type = np.number,na_number = 50):
+    """
+    Filters the input DataFrame based on specified conditions: selects columns
+    of a specific data type, drops columns with a number of NaN values above a
+    certain threshold, and removes the 'Age' column.
+
+    Parameters:
+    - df: DataFrame to be filtered.
+    - data_type: Data type used to filter the columns. Default is numeric data types.
+    - na_number: Threshold for dropping columns based on the count of NaN values.
+
+    Returns:
+    - A filtered copy of the input DataFrame.
+    """
+    # Select columns of the specified data type from the DataFrame
     numeric_columns = df.select_dtypes(include=[data_type]).columns
     columns_to_select = ['EPRIME_CODE'] + list(numeric_columns)
 
-
-    # Selecting all numeric columns along with "Subject"
+    # Filter out rows where 'clusters' value is negative
     df = df[df["clusters"] >= 0]
 
+    # Select only the relevant columns
     filtered_df = df[columns_to_select]
 
-
+    # Identify columns to drop based on the threshold of NaN values
     cols_to_drop = filtered_df.columns[filtered_df.isnull().sum() > na_number]
+    # Drop identified columns
     filtered_df = filtered_df.drop(columns=cols_to_drop).copy()
+    # Drop 'Age' column
     filtered_df.drop("Age", axis=1, inplace=True)
 
     return filtered_df
 
 
 def create_metrics_cluster_df(df_cluster, df_scales):
+    """
+    Merges two DataFrames on 'Subject ID' (for df_cluster) and 'EPRIME_CODE' (for
+    df_scales), filters the merged DataFrame, and computes mean values for each
+    cluster and metric. It constructs a new DataFrame with these computed means,
+    organized by cluster.
+
+    Parameters:
+    - df_cluster: DataFrame containing cluster assignments.
+    - df_scales: DataFrame containing scale scores.
+
+    Returns:
+    - A DataFrame with mean values for each metric, organized by cluster.
+    """
+    # Merge cluster assignments with scale scores on their common identifiers
     new_data = pd.merge(df_cluster, df_scales, left_on='Subject ID', right_on='EPRIME_CODE')
 
+    # Filter the merged DataFrame
     filter_df = filter_data(new_data)
 
+    # Initialize a dictionary to hold the cluster metrics
     data_clust = {}
+    # Iterate over each unique cluster to calculate metrics
     for clust in np.unique(filter_df["clusters"]):
         metric_data = {}
+        # Calculate mean for each column (excluding 'EPRIME_CODE' and 'clusters')
         for column in filter_df.drop(["EPRIME_CODE", "clusters"], axis=1).columns:
             mean_col = filter_df[filter_df["clusters"] == clust][column].mean()
-            # std_col = filter_df[filter_df["clusters"] == clust][column].std()
-            # max_col = filter_df[filter_df["clusters"] == clust][column].max()
-            # min_col = filter_df[filter_df["clusters"] == clust][column].min()
+            # Store the mean value
+            metric_data[column] = {'mean': round(mean_col, 2)}
 
-            # Normality Test
-            # _, p_value_col = stats.shapiro(result_df[result_df["clusters"]==clust][column])
-            metric_data[column] = {'mean': round(mean_col, 2)}  # , 'std': round(std_col, 2),
-            # 'max': max_col, 'min': min_col}
-
+        # Add the metrics data to the main dictionary
         data_clust[clust] = metric_data
 
+    # Create a DataFrame to hold the organized metrics data
     df_clusters = pd.DataFrame()
+    # Populate the DataFrame with the metrics data
     for cluster, attributes in data_clust.items():
         for attribute, values in attributes.items():
             for stat, value in values.items():
@@ -57,21 +88,34 @@ def create_metrics_cluster_df(df_cluster, df_scales):
 
 
 def create_mean_tasks(df, df_cluster):
+    """
+    Merges task scores with cluster assignments, computes mean scores for
+    each task category within each cluster, and organizes the results in a
+    new DataFrame.
+
+    Parameters:
+    - df: DataFrame containing task scores.
+    - df_cluster: DataFrame containing cluster assignments.
+
+    Returns:
+    - A DataFrame with mean scores for each task category, organized by cluster.
+    """
+    # Copy the original DataFrame
     copy_data = df.copy()
+    # Assign cluster labels to the copied DataFrame
     copy_data["clusters"] = df_cluster["clusters"]
 
-    # Extract cluster labels
+    # Extract unique cluster labels
     clusters = copy_data['clusters'].unique()
 
-    # Create an empty DataFrame to store the mean scores
+    # Initialize an empty DataFrame to store mean scores
     df_mean_scores = pd.DataFrame(columns=['Cluster', 'Happy_0', 'Happy_1', 'Sad_0', 'Sad_1', 'Fear_0', 'Fear_1'])
 
-    # Iterate over each cluster
+    # Calculate mean scores for each task category within each cluster
     for cluster in clusters:
-        # Filter data for the current cluster
         cluster_data = copy_data[copy_data['clusters'] == cluster]
-
-        # Calculate mean scores for each category
+        # Calculate the mean from each individual for each category, and then the mean of all individuals
+        # from the same cluster
         df_mean_scores.loc[len(df_mean_scores)] = [
             cluster,
             cluster_data[['Happy_0_' + str(i) for i in range(6)]].mean().mean(),
@@ -82,47 +126,79 @@ def create_mean_tasks(df, df_cluster):
             cluster_data[['Fear_1_' + str(i) for i in range(9)]].mean().mean()
         ]
 
-    # Display the mean scores for each cluster
     return df_mean_scores
 
 
 
 def create_word(df, list_metrics, doc_name):
+    """
+    Creates a Word document containing groups of metrics, with each group
+    including boxplots and statistical tables for each variable. Plots and tables
+    are generated, added to the document, and then the corresponding files are
+    deleted.
+
+    Parameters:
+    - df: DataFrame containing the data to be visualized and analyzed.
+    - list_metrics: List of metric groups, where each group is a list of variable names.
+    - doc_name: Name of the output Word document file.
+
+    Note: This function relies on external functions `create_boxplot` and `create_stats_table`
+    for generating plots and tables, and requires 'sns' (Seaborn), 'Document' from 'docx',
+    and 'Inches' from 'docx.shared' to be imported and configured appropriately.
+    """
     sns.set(style="whitegrid")
+    # Initialize a new Word document
     doc = Document()
+    # Calculate the size of each cluster
     size_clust = np.unique(df["clusters"], return_counts=True)[1]
 
+    # Loop through each group of metrics
     for idx, group in enumerate(list_metrics):
-        # Add group name to the document
-
+        # Add a paragraph for each group to the document
         doc.add_paragraph(f"Group {idx + 1}: {', '.join(group)}")
 
         for i, variable in enumerate(group):
+            # Prepare the data for plotting
             col_list = [variable, 'clusters']
             variable_df = df[col_list]
             melted_df = pd.melt(variable_df, id_vars=['clusters'], value_vars=variable,
                                 var_name='Variable', value_name='Mean_Score')
 
+            # Generate a boxplot and a statistics table, and save them as images
             plot_filename = predictive_plots.create_boxplot(melted_df, variable, save_plot=True, index=[idx, i])
+            table_filename = predictive_plots.create_stats_table(melted_df, variable, size_clust, save_plot=True,
+                                                                 index=[idx, i])
 
-            table_filename = predictive_plots.create_stats_table(melted_df, variable, size_clust, save_plot=True, index=[idx, i])
-
-            # Add plot and table to the Word document in the same row
+            # Add the images to the Word document
             table_cell = doc.add_table(rows=1, cols=2)
             table_row = table_cell.rows[0]
-            cell1 = table_row.cells[0]
-            cell2 = table_row.cells[1]
+            cell1, cell2 = table_row.cells
+            cell1.paragraphs[0].add_run().add_picture(plot_filename, width=Inches(3))
+            cell2.paragraphs[0].add_run().add_picture(table_filename, width=Inches(3))
 
-            cell1.paragraphs[0].add_run().add_picture(plot_filename, width=Inches(3))  # Adjust width as needed
-            cell2.paragraphs[0].add_run().add_picture(table_filename, width=Inches(3))  # Adjust width as
-
+            # Remove the image files after adding them to the document
             os.remove(plot_filename)
             os.remove(table_filename)
+
+    # Save the Word document
     doc.save(doc_name)
 
 
 def export_clusters_df(df,output_name):
+    """
+    Processes the input DataFrame to set specific column names, sorts by 'EPRIME_CODE',
+    resets the index, and exports the DataFrame to an Excel file.
 
+    Parameters:
+    - df: DataFrame containing 'EPRIME_CODE' and cluster assignments.
+    - output_name: Name of the output Excel file.
+
+    Note: This function modifies the column names of the input DataFrame and assumes
+    the existence of columns that need to be renamed to 'EPRIME_CODE' and 'clusters'.
+    """
+    # Rename columns to standard names
     df.columns = ["EPRIME_CODE", "clusters"]
+    # Sort DataFrame by 'EPRIME_CODE' and reset index
     df = df.sort_values(by='EPRIME_CODE').reset_index(drop=True)
+    # Export the DataFrame to an Excel file
     df.to_excel(output_name, index=False)
